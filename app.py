5e5de3e83c997934d8eb2ba58241e4a292d9e45a
Soumyabroto Banerjee <soumyabroto.banerjee@gmail.com>
2023-10-30 18:24:02 +0530

    hello

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Dash(__name__)
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Fruit": ["Apples", "Grapes", "Bananas", "Apples", "Oranges", "Bananas"],
     "Amount": [4, 1, 2, 2, 4, 5],
     "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
 })
@@ -19,7 +19,7 @@ df = pd.DataFrame({
 fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
 
 app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
+    html.H1(children='Hello'),
 
     html.Div(children='''
         Dash: A web application framework for your data.
@@ -32,4 +32,4 @@ app.layout = html.Div(children=[
 ])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
