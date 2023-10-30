704f0d396757cad3db54ef492adbb99cf1fc55a3
Soumyabroto Banerjee <soumyabroto.banerjee@gmail.com>
2023-10-30 23:28:43 +0530

    hello updated v2

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ app = Flask(__name__)
 # CSV file configuration
 csv_file = 'tasks.csv'
 
-def create_csv():
-    pd.DataFrame(columns=['id', 'description', 'done']).to_csv(csv_file, index=False)
+# def create_csv():
+#     pd.DataFrame(columns=['id', 'description', 'done']).to_csv(csv_file, index=False)
 
 @app.route('/')
 def index():
-    create_csv()  # Create the CSV file if it doesn't exist
+    # create_csv()  # Create the CSV file if it doesn't exist
     tasks = pd.read_csv(csv_file)
     return render_template('index.html', tasks=tasks.values)
 
@@ -29,18 +29,26 @@ def index():
 def add_task():
     description = request.form['description']
     tasks = pd.read_csv(csv_file)
-    new_task = pd.DataFrame({'description': [description], 'done': [0]})
+
+    # Find the maximum ID value and increment it to ensure uniqueness
+    max_id = tasks['id'].max() if not tasks.empty else 0
+    new_id = max_id + 1
+
+    new_task = pd.DataFrame({'id': [new_id], 'description': [description], 'done': [0]})
     tasks = tasks.append(new_task, ignore_index=True)
     tasks.to_csv(csv_file, index=False)
     return redirect(url_for('index'))
 
+
 @app.route('/toggle_task/<int:task_id>')
 def toggle_task(task_id):
     tasks = pd.read_csv(csv_file)
-    if task_id in tasks['id'].values:
-        task = tasks.loc[tasks['id'] == task_id]
-        tasks.at[task.index[0], 'done'] = int(not task['done'])
+    task = tasks.loc[tasks['id'] == task_id]
+
+    if not task.empty:
+        tasks.at[task.index[0], 'done'] = int(not task['done'].values[0])
         tasks.to_csv(csv_file, index=False)
+
     return redirect(url_for('index'))
 
 @app.route('/delete_task/<int:task_id>')
