fc8ff143461b4c66d5bcb23f23af5c4521aa6269
Soumyabroto Banerjee <soumyabroto.banerjee@gmail.com>
2023-10-30 18:50:36 +0530

    heelo1

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # Run this app with `python app.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-
+from flask import Flask
 from dash import Dash, html, dcc
 import plotly.express as px
 import pandas as pd
-
-app = Dash(__name__)
+flask_server = Flask(__name__)
+app = Dash(__name__,server=flask_server)
 
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
